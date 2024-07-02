9e7ad563a33d0ae9916268cf1dfa2f8d51e5a9d8
Akaash Kamdar <kamdarakaash@gmail.com>
2024-07-02 11:14:15 -0400

    Setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='akaash',
+      version='0.1',
+      url='http://github.com/akaashkamdar/DS5100-packagepractice',
+      author='Who knows?',
+      author_email='we_shall_see@gmail.com',
+      license='MIT',
+      packages=['akaash']
+      )
